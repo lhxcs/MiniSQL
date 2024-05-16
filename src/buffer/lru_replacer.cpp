e7688a0b118f990ef4a20d579e2c0e16307f9833
lhxcs <lihanxuan23@gmail.com>
2024-05-16 21:56:15 +0800

    finish lru_replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -1,4 +1,5 @@
 #include "buffer/lru_replacer.h"
+#include "glog/logging.h"
 
 LRUReplacer::LRUReplacer(size_t num_pages){}
 
@@ -8,26 +9,40 @@ LRUReplacer::~LRUReplacer() = default;
  * TODO: Student Implement
  */
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
-  return false;
+  if(lru_list_.size() == 0) {
+    return false;
+  }
+  // LOG(INFO) << "victim's id " << *frame_id;
+  for(auto it = lru_list_.begin(); it != lru_list_.end(); it++) {
+    *frame_id = *it;
+  }
+  lru_list_.erase(*frame_id);
+  return true;
 }
 
 /**
  * TODO: Student Implement
  */
 void LRUReplacer::Pin(frame_id_t frame_id) {
-
+  if(lru_list_.find(frame_id) != lru_list_.end()) {
+    lru_list_.erase(frame_id);
+  }
 }
 
 /**
  * TODO: Student Implement
  */
 void LRUReplacer::Unpin(frame_id_t frame_id) {
-
+  if(lru_list_.find(frame_id) == lru_list_.end()) {
+    lru_list_.insert(frame_id);
+  }
+  
+  //LOG(INFO) << "current lru_list size " << lru_list.size();
 }
 
 /**
  * TODO: Student Implement
  */
 size_t LRUReplacer::Size() {
-  return 0;
+  return lru_list_.size();
 }
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -37,6 +37,7 @@ class LRUReplacer : public Replacer {
 
 private:
   // add your own private member variables here
+  unordered_set <frame_id_t> lru_list_;
 };
 
 #endif  // MINISQL_LRU_REPLACER_H
