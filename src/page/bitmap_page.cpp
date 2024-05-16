1032811d3c0c43771b11a676d3281c98c0ce231f
lhxcs <lihanxuan23@gmail.com>
2024-05-16 21:50:20 +0800

    finish bitmap_page

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -7,7 +7,23 @@
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  return false;
+  if(page_allocated_ >= this->GetMaxSupportedSize()) {
+    return false;
+  }
+  page_offset = next_free_page_;
+  uint32_t byte_index = page_offset / 8;
+  uint8_t bit_index = page_offset % 8;
+  bytes[byte_index] |= (1 << bit_index);
+  page_allocated_++;
+  for(uint32_t i = 0; i < this->GetMaxSupportedSize(); i++) {
+    byte_index = i / 8;
+    bit_index = i % 8;
+    if(IsPageFreeLow(byte_index, bit_index)) {
+      next_free_page_ = i;
+      break;
+    }
+  }
+  return true;
 }
 
 /**
@@ -15,7 +31,21 @@ bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  return false;
+  if(page_offset >= this->GetMaxSupportedSize()) {
+    return false;
+  }
+  uint32_t byte_index = page_offset / 8;
+  uint8_t bit_index = page_offset % 8;
+  
+  if((IsPageFreeLow(byte_index, bit_index))) {
+    return false;
+  }
+  bytes[byte_index] &= ~(1 << bit_index);
+  page_allocated_--;
+  if(page_offset < next_free_page_) {
+    next_free_page_ = page_offset;
+  }
+  return true;
 }
 
 /**
@@ -23,12 +53,17 @@ bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-  return false;
+  if(page_offset >= this->GetMaxSupportedSize()) {
+    return false;
+  }
+  uint32_t byte_index = page_offset / 8;
+  uint8_t bit_index = page_offset % 8;
+  return IsPageFreeLow(byte_index, bit_index);
 }
 
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFreeLow(uint32_t byte_index, uint8_t bit_index) const {
-  return false;
+  return (bytes[byte_index] & (1 << bit_index)) == 0;
 }
 
 template class BitmapPage<64>;
